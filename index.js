fb86bb77ea5230bd25a76a15eafa54cf4da7bf7f
GustaLeao <leao.gustavo075@gmail.com>
2024-04-21 20:43:16 -0300

    added checkbox for header nav in widths with less than 580px

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-var elementosDuvida = document.querySelectorAll('.duvida')
+var elementosDuvida = document.querySelectorAll(".duvida");
+const headerNav = document.getElementById("header_nav");
 
 elementosDuvida.forEach(function (duvida) {
-    duvida.addEventListener('click', function() {
-        duvida.classList.toggle ('ativa')
-    })
-})
+  duvida.addEventListener("click", function () {
+    duvida.classList.toggle("ativa");
+  });
+});
+const verifyResolution = () => {
+  if (window.innerWidth < 580) {
+    headerNav.innerHTML =
+      '<input type="checkbox" id="navigation_button"></input>';
+
+    /*var links = ['inicio', 'farmacia', 'clinica', 'duvidas'];
+    const linksContent = ['Início', 'Farmácia', 'Clínica', 'Dúvidas']
+
+    for(i=0;i<links.length;i++){
+        var a = document.createElement('a');
+        a.href = `#${links[i]}`; // Você pode definir o href conforme necessário
+        a.textContent = linksContent[i];
+        headerNav.appendChild(a);
+    }
+    console.log()
+    */
+
+    /* Código experimental para adicionar os links no headerNav
+    links.forEach(function(link) {
+        var a = document.createElement('a');
+        a.id = link;
+        a.href = `#${links}`; // Você pode definir o href conforme necessário
+        a.textContent = linksContent;
+        headerNav.appendChild(a);
+    });*/
+  }
+};
+
+window.onload = verifyResolution;
+window.onresize = verifyResolution;
